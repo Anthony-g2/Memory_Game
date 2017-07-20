1967c734aa93cb582439390c8f7344fc3bfe8ad2
Anthony Greenwell <ant.greenwell2@gmail.com>
2017-07-20 13:52:36 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("All systems are go!")
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("Player flipped " + cardOne)
+console.log("Player flipped " + cardFour)
